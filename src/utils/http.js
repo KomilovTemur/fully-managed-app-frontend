e0ba237838e0c91af4fea492b58811b938c6be6e
Temur Komilov <itteamdev74@gmail.com>
2023-02-03 10:47:32 +0500

    fixing some errors

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
   baseURL: "http://localhost:4444",
-  headers: {
-    'Authorization': localStorage.getItem("token")
-  }
 })
 
 export default instance
